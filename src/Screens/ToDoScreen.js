709a10b899515c683616d78dad6ff28912787973
Krishna Sharma <118767245+Krishna6438@users.noreply.github.com>
2024-07-16 14:52:44 +0530

    Add files via upload

---
+++ src/Screens/ToDoScreen.js
@@ -0,0 +1,53 @@
+// import { Component } from "react";
+
+import { useState } from "react";
+import Task from "../components/Task";
+
+// class ToDoScreen extends Component{
+
+//     state={
+//         taskCounter : 0,
+//     };
+//     render(){
+//         return(
+
+//             <div className="screen">
+//                 <h1 className="ui heading center">ToDo List</h1>
+//                 <div>
+//                     <button className="ui secondary button" onClick={(e)=>{
+//                         this.setState({...this.state,taskCounter:this.state.taskCounter+1})
+//                     }}>Add Task</button>
+//                     <p>The number of tasks are:{this.state.taskCounter}</p>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+
+const ToDoScreen = () => {
+    const [taskList, setTaskList] = useState([]);
+    return (
+
+        <div className="screen">
+            <h1 className="ui heading center">ToDo List</h1>
+            <div>
+                <button className="ui secondary button" onClick={(e) => {
+                    setTaskList([...taskList, {
+                        title: "Go to gym",
+                        description: "Do 30 minutes of cardio",
+                        createDate: new Date()
+                    }])
+                }}>Add Task</button>
+                <section>
+                    <div className="ui cards">
+                        {taskList.map((task) =>
+                             <Task task={task}/>)}     
+                    </div>
+                </section>
+
+            </div>
+        </div>
+    )
+}
+
+export default ToDoScreen;
